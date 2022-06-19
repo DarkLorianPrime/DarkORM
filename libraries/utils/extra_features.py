98df42aef36a8a296c83903514533a007715b5ba
darklorianprime <lolpokens@mail.ru>
2022-06-19 21:40:56 +0400

    Update readme. Re-writted ORM.

--- libraries/utils/extra_features.py
+++ libraries/utils/extra_features.py
@@ -1,10 +1,11 @@
 class Singleton(object):
-    _instance = None
+    __instance = None
 
     def __new__(cls):
         """
         Создает новый объект класса если такового не существует, или же просто возвращает существующий
         """
-        if not cls._instance:
-            cls._instance = object.__new__(cls)
-        return cls._instance
+        if not cls.__instance:
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
+
